6c1ed6e6cd11ed05bdad64ba471aca556c24f4ca
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-06-16 10:15:17 +0200

    FIX app project page

--- src/pages/AppProjectPage.vue
+++ src/pages/AppProjectPage.vue
@@ -1,15 +1,3 @@
-<template>
-    <div class="container my-5">
-        <div class="text-end p-0 m-0" v-if="project.type"><span class="badge text-bg-info">{{ project.type.name }}</span>
-        </div>
-        <h1>{{ project.title }}</h1>
-        <p>{{ project.description }}</p>
-        <div class="d-flex gap-1" v-if="project.tecnologies">
-            <span class="badge bg-secondary" v-for="tecnology in project.tecnologies">{{ tecnology.name }}</span>
-        </div>
-    </div>
-</template>
-
 <script>
 import axios from 'axios';
 
@@ -40,3 +28,16 @@ export default {
     }
 }
 </script>
+
+<template>
+    <div class="container my-5">
+        <div class="text-end p-0 m-0" v-if="project.type"><span class="badge text-bg-info">{{ project.type.name }}</span>
+        </div>
+        <h1>{{ project.title }}</h1>
+        <p>{{ project.description }}</p>
+        <div class="d-flex gap-1" v-if="project.technologies">
+            <span class="badge bg-secondary" v-for="technology in project.technologies">{{ technology.name }}</span>
+        </div>
+    </div>
+</template>
+
